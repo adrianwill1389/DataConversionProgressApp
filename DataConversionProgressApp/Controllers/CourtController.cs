fc09dccdb18b5e954fa56c3d318a45b60aa5ea56
Adrian Williams <adrianwilliams1389@gmail.com>
2025-06-21 14:02:08 -0500

    Add project files.

---
+++ DataConversionProgressApp/Controllers/CourtController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DataConversionProgressApp.Models;
+using System.Collections.Generic;
+using DataConversionProgressApp.Models;
+
+public class CourtController : Controller
+{
+    public IActionResult Index()
+    {
+        var sampleList = new List<CourtProgress>
+        {
+            new CourtProgress { DateReceived = new DateTime(2025,5,1), Court1Disposed = true, Court1Warrant = true, Court2CaseManagement = true, Court2Warrant = true, User = "Admin" },
+            new CourtProgress { DateReceived = new DateTime(2025,5,2), Court1Disposed = true, Court1Warrant = true, Court2CaseManagement = true, Court2Warrant = true, User = "Clerk1" },
+            new CourtProgress { DateReceived = new DateTime(2025,5,5), Court1Disposed = true, Court1Warrant = true, Court2CaseManagement = true, Court2Warrant = false, User = "Clerk2" },
+            // Add more entries like your Excel
+        };
+
+        return View(sampleList);
+    }
+}
---
+++ DataConversionProgressApp/Models/CourtProgress.cs
@@ -0,0 +1,30 @@
+﻿namespace DataConversionProgressApp.Models
+{
+    public class CourtProgress
+    {
+        internal bool Court2CaseManagement;
+
+        public DateTime DateReceived { get; set; }
+        public bool Court1Disposed { get; set; }
+        public bool Court1Warrant { get; set; }
+        public bool Court2Disposed { get; set; }
+        public bool Court2Warrant { get; set; }
+
+        public int Progress
+        {
+            get
+            {
+                int count = 0;
+                if (Court1Disposed) count++;
+                if (Court1Warrant) count++;
+                if (Court2Disposed) count++;
+                if (Court2Warrant) count++;
+                return (int)(count / 4.0 * 100);
+            }
+        }
+
+        public string Status => Progress == 100 ? "Completed" : "In Progress";
+        public string User { get; set; }  // Who checked it
+    }
+
+}
